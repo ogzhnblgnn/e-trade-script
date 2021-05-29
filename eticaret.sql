cfebc6d55673311c627e6c07d3aabd739eac03c7
ogzhnbxx <78086858+ogzhnbxx@users.noreply.github.com>
2021-05-29 22:02:13 +0300

    Database

---
+++ eticaret.sql
@@ -0,0 +1,235 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Anamakine: localhost
+-- Üretim Zamanı: 29 May 2021, 19:01:32
+-- Sunucu sürümü: 8.0.17
+-- PHP Sürümü: 7.3.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Veritabanı: `eticaret`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `ayar`
+--
+
+CREATE TABLE `ayar` (
+  `ayar_id` int(1) NOT NULL,
+  `ayar_logo` varchar(250) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_title` varchar(250) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_description` varchar(250) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_keywords` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_author` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_tel` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_gsm` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_fax` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_mail` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_ilce` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_il` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_adres` varchar(250) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_mesai` varchar(250) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_maps` varchar(250) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_analystic` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_zopim` varchar(250) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_facebook` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_twitter` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_google` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_youtube` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_smtphost` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_smtpuser` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_smtppassword` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_smtpport` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_bakim` enum('0','1') COLLATE utf8_turkish_ci NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+--
+-- Tablo döküm verisi `ayar`
+--
+
+INSERT INTO `ayar` (`ayar_id`, `ayar_logo`, `ayar_title`, `ayar_description`, `ayar_keywords`, `ayar_author`, `ayar_tel`, `ayar_gsm`, `ayar_fax`, `ayar_mail`, `ayar_ilce`, `ayar_il`, `ayar_adres`, `ayar_mesai`, `ayar_maps`, `ayar_analystic`, `ayar_zopim`, `ayar_facebook`, `ayar_twitter`, `ayar_google`, `ayar_youtube`, `ayar_smtphost`, `ayar_smtpuser`, `ayar_smtppassword`, `ayar_smtpport`, `ayar_bakim`) VALUES
+(0, 'dimg/22737logo.jpeg', 'E-Ticaret Proje Örneği', 'E-Ticaret proje çalışma örneği', 'e-ticaret, shopping, phpss', 'Oğuzhan Bilgin', '(+90) 545 385 59 98', '(+90) 545 385 59 98', '(+90)545 385 59 98', 'ogzhnbxx@gmail.com', 'Canik', 'Samsun', 'Hasköy mh. Karaçam Sk. Damla Elit C-1', '7 x 24 açık E-Ticaret Scripti', 'ayar_maps_api', 'ayar_analystic_api', 'ayar_zopim_api', 'www.facebook.com', 'www.twitter.com', 'www.google.com', 'www.youtube.com', 'mail.alanadiniz.com', 'smtpuser', 'password', '587', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `hakkimizda`
+--
+
+CREATE TABLE `hakkimizda` (
+  `hakkimizda_id` int(1) NOT NULL,
+  `hakkimizda_baslik` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `hakkimizda_icerik` text COLLATE utf8_turkish_ci NOT NULL,
+  `hakkimizda_video` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `hakkimizda_vizyon` text COLLATE utf8_turkish_ci NOT NULL,
+  `hakkimizda_misyon` text COLLATE utf8_turkish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+--
+-- Tablo döküm verisi `hakkimizda`
+--
+
+INSERT INTO `hakkimizda` (`hakkimizda_id`, `hakkimizda_baslik`, `hakkimizda_icerik`, `hakkimizda_video`, `hakkimizda_vizyon`, `hakkimizda_misyon`) VALUES
+(0, 'Hakkımızda Alanı Başlık', '<p><img alt=\"\" src=\"https://upload.wikimedia.org/wikipedia/commons/2/27/PHP-logo.svg\" style=\"height:50px; width:93px\" /><em><strong>&nbsp;ile admin panelli e-ticaret proje &ouml;rneği.</strong></em></p>\r\n', 'nnXhnvmRhp4', 'Vizyon içerik', 'Misyon içerik');
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `kullanici`
+--
+
+CREATE TABLE `kullanici` (
+  `kullanici_id` int(11) NOT NULL,
+  `kullanici_zaman` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `kullanici_resim` varchar(250) COLLATE utf8_turkish_ci NOT NULL,
+  `kullanici_tc` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `kullanici_ad` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `kullanici_mail` varchar(100) COLLATE utf8_turkish_ci NOT NULL,
+  `kullanici_gsm` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `kullanici_sifre` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `kullanici_adsoyad` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `kullanici_adres` varchar(250) COLLATE utf8_turkish_ci NOT NULL,
+  `kullanici_il` varchar(100) COLLATE utf8_turkish_ci NOT NULL,
+  `kullanici_ilce` varchar(100) COLLATE utf8_turkish_ci NOT NULL,
+  `kullanici_unvan` varchar(100) COLLATE utf8_turkish_ci NOT NULL,
+  `kullanici_yetki` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `kullanici_durum` int(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+--
+-- Tablo döküm verisi `kullanici`
+--
+
+INSERT INTO `kullanici` (`kullanici_id`, `kullanici_zaman`, `kullanici_resim`, `kullanici_tc`, `kullanici_ad`, `kullanici_mail`, `kullanici_gsm`, `kullanici_sifre`, `kullanici_adsoyad`, `kullanici_adres`, `kullanici_il`, `kullanici_ilce`, `kullanici_unvan`, `kullanici_yetki`, `kullanici_durum`) VALUES
+(1, '2021-05-16 01:49:18', '', '', '', 'ogzhnbxx@gmail.com', '0545 385 59 98', 'e10adc3949ba59abbe56e057f20f883e', 'Oğuzhan Bilgin', '', '', '', '', '5', 1),
+(6, '2021-05-22 12:42:55', '', '', '', 'sadsadas@asdasd', '', '303afa47adc42a3aa87fcfedbbdf2652', 'sadsads', '', '', '', '', '1', 1),
+(7, '2021-05-22 13:04:45', '', '', '', 'camcihuseyin@gmail.com', '', 'd6ea4aee03f5808b2cf7d78c88c8c2d0', 'Camcı Hüseyin', '', '', '', '', '1', 1),
+(8, '2021-05-22 13:07:09', '', '', '', 'zinzin@gmail.comss', '65464654654', 'e10adc3949ba59abbe56e057f20f883e', '', '', '', '', '', '1', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `menu`
+--
+
+CREATE TABLE `menu` (
+  `menu_id` int(11) NOT NULL,
+  `menu_ust` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `menu_ad` varchar(100) COLLATE utf8_turkish_ci NOT NULL,
+  `menu_detay` text COLLATE utf8_turkish_ci NOT NULL,
+  `menu_url` varchar(250) COLLATE utf8_turkish_ci NOT NULL,
+  `menu_sira` int(2) NOT NULL,
+  `menu_durum` enum('0','1') COLLATE utf8_turkish_ci NOT NULL,
+  `menu_seourl` varchar(250) COLLATE utf8_turkish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+--
+-- Tablo döküm verisi `menu`
+--
+
+INSERT INTO `menu` (`menu_id`, `menu_ust`, `menu_ad`, `menu_detay`, `menu_url`, `menu_sira`, `menu_durum`, `menu_seourl`) VALUES
+(1, '0', 'Hakkımızda', '', 'hakkimizda', 1, '1', 'hakkimizda'),
+(5, '0', 'Kategoriler', '<p>asşldjasdaslkdjaskljdkwq</p>\r\n', '', 2, '1', 'kategoriler'),
+(6, '0', 'İletişim', '', '', 3, '1', 'iletisim'),
+(8, '', 'Gizlilik Koşulları', '', '', 0, '1', 'gizlilik-kosullari'),
+(11, '', 'Mesafeli Satış Sözleşmesi', '<p>MESAFELİ S&Ouml;ZLEŞMELER Y&Ouml;NETMELİĞİ</p>\r\n\r\n<p>BİRİNCİ B&Ouml;L&Uuml;M</p>\r\n\r\n<p>Ama&ccedil;, Kapsam, Dayanak ve Tanımlar</p>\r\n\r\n<p><strong>Ama&ccedil;</strong></p>\r\n\r\n<p><strong>MADDE 1 &ndash;</strong>&nbsp;(1) Bu Y&ouml;netmeliğin amacı, mesafeli s&ouml;zleşmelere ilişkin uygulama usul ve esaslarını d&uuml;zenlemektir.</p>\r\n\r\n<p><strong>Kapsam</strong></p>\r\n\r\n<p><strong>MADDE 2 &ndash;</strong>&nbsp;(1) Bu Y&ouml;netmelik, mesafeli s&ouml;zleşmelere uygulanır.</p>\r\n\r\n<p>(2) Bu Y&ouml;netmelik h&uuml;k&uuml;mleri;</p>\r\n\r\n<p>a) Finansal hizmetler,</p>\r\n\r\n<p>b) Otomatik makineler aracılığıyla yapılan satışlar,</p>\r\n\r\n<p>c) Halka a&ccedil;ık telefon vasıtasıyla telekom&uuml;nikasyon operat&ouml;rleriyle bu telefonun kullanımı,</p>\r\n\r\n<p>&ccedil;) Bahis, &ccedil;ekiliş, piyango ve benzeri şans oyunlarına ilişkin hizmetler,</p>\r\n\r\n<p>d) Taşınmaz malların veya bu mallara ilişkin hakların oluşumu, devri veya kazanımı,</p>\r\n\r\n<p>e) Konut kiralama,</p>\r\n\r\n<p>f) Paket turlar,</p>\r\n\r\n<p>g) Devre m&uuml;lk, devre tatil, uzun s&uuml;reli tatil hizmeti ve bunların yeniden satımı veya değişimi,</p>\r\n\r\n<p>ğ) Yiyecek ve i&ccedil;ecekler gibi g&uuml;nl&uuml;k t&uuml;ketim maddelerinin, satıcının d&uuml;zenli teslimatları &ccedil;er&ccedil;evesinde t&uuml;keticinin meskenine veya işyerine g&ouml;t&uuml;r&uuml;lmesi,</p>\r\n\r\n<p>h) 5 inci maddenin birinci fıkrasının (a), (b) ve (d) bentlerindeki bilgi verme y&uuml;k&uuml;ml&uuml;l&uuml;ğ&uuml; ile 18 inci ve 19 uncu maddelerde yer alan y&uuml;k&uuml;ml&uuml;l&uuml;kler saklı kalmak koşuluyla yolcu taşıma hizmetleri,</p>\r\n\r\n<p>ı) Malların montaj, bakım ve onarımı,</p>\r\n\r\n<p>i) Bakımevi hizmetleri, &ccedil;ocuk, yaşlı ya da hasta bakımı gibi ailelerin ve kişilerin desteklenmesine y&ouml;nelik sosyal hizmetler</p>\r\n\r\n<p>ile&nbsp;ilgili s&ouml;zleşmelere uygulanmaz.</p>\r\n', '', 20, '1', 'mesafeli-satis-sozlesmesi');
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `slider`
+--
+
+CREATE TABLE `slider` (
+  `slider_id` int(11) NOT NULL,
+  `slider_ad` varchar(100) COLLATE utf8_turkish_ci NOT NULL,
+  `slider_yol` varchar(250) COLLATE utf8_turkish_ci NOT NULL,
+  `slider_sira` int(2) NOT NULL,
+  `slider_link` varchar(250) COLLATE utf8_turkish_ci NOT NULL,
+  `slider_durum` enum('0','1') COLLATE utf8_turkish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+--
+-- Tablo döküm verisi `slider`
+--
+
+INSERT INTO `slider` (`slider_id`, `slider_ad`, `slider_yol`, `slider_sira`, `slider_link`, `slider_durum`) VALUES
+(7, 'Slider1', 'dimg/slider/30165245512321125451slide-1.jpg', 1, '', '1'),
+(8, 'Slider2', 'dimg/slider/28759233333097820023slide-2.jpg', 2, '', '1'),
+(9, 'Slider3', 'dimg/slider/31763248032943024484slide-3.jpg', 3, '', '1'),
+(10, 'Slider4', 'dimg/slider/24901301472156030772slide-4.jpg', 4, '', '0');
+
+--
+-- Dökümü yapılmış tablolar için indeksler
+--
+
+--
+-- Tablo için indeksler `ayar`
+--
+ALTER TABLE `ayar`
+  ADD PRIMARY KEY (`ayar_id`);
+
+--
+-- Tablo için indeksler `hakkimizda`
+--
+ALTER TABLE `hakkimizda`
+  ADD PRIMARY KEY (`hakkimizda_id`);
+
+--
+-- Tablo için indeksler `kullanici`
+--
+ALTER TABLE `kullanici`
+  ADD PRIMARY KEY (`kullanici_id`);
+
+--
+-- Tablo için indeksler `menu`
+--
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`menu_id`);
+
+--
+-- Tablo için indeksler `slider`
+--
+ALTER TABLE `slider`
+  ADD PRIMARY KEY (`slider_id`);
+
+--
+-- Dökümü yapılmış tablolar için AUTO_INCREMENT değeri
+--
+
+--
+-- Tablo için AUTO_INCREMENT değeri `kullanici`
+--
+ALTER TABLE `kullanici`
+  MODIFY `kullanici_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- Tablo için AUTO_INCREMENT değeri `menu`
+--
+ALTER TABLE `menu`
+  MODIFY `menu_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- Tablo için AUTO_INCREMENT değeri `slider`
+--
+ALTER TABLE `slider`
+  MODIFY `slider_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
